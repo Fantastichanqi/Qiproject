eb2b8e0b12514f8541fa5d2c08c7022d1b53edda
Hanqi <163219730+Fantastichanqi@users.noreply.github.com>
2025-03-23 09:33:18 +0800

    Add files via upload
    The first day of self-studying the C programming language~Lesson1

---
+++ 1/1.3hello/cmake-build-debug/CMakeFiles/1_3hello.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1_3hello.exe"
+  "1_3hello.exe.manifest"
+  "1_3hello.pdb"
+  "CMakeFiles/1_3hello.dir/main.c.obj"
+  "lib1_3hello.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/1_3hello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 1/1.6/Helloworld/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>    #头文件 对于文件的声明
+
+int main() {
+    int i=10 ;
+    printf("Hello, World!\n");
+    return 0;
+}
